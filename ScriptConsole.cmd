6e002684e66872b2fc7ef3fb5812e7025802bb98
baimuwu888 <32180308+baimuwu888@users.noreply.github.com>
2022-08-19 10:05:10 +0800

    添加了几行注释

--- ScriptConsole.cmd
+++ ScriptConsole.cmd
@@ -2,8 +2,9 @@
 
 rem Find MSBuild
 setlocal enabledelayedexpansion
+rem 找到vswhere.exe的地址 
 set VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-
+rem 找到csi.exe的地址 csi是用来执行C#脚本的
 for /f "usebackq tokens=*" %%i in (`%VSWHERE% -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\Roslyn\csi.exe`) do (
   set CSIPATH=%%i
 )
@@ -15,11 +16,13 @@ if not exist "%CSIPATH%" (
 )
 
 rem Init environment variables
+rem 设置.csx为可执行的脚本
 set PATHEXT=%PATHEXT%;.CSX
+rem设置当前文件夹路径
 set MMROOT=%~dp0
-
+rem 进入到Build文件夹
 cd %MMROOT%\Build
-
+rem 再启动一个新的窗口
 rem Start
 if not %1.==. (
 	set CMD=%*
@@ -43,7 +46,7 @@ echo -
 
 :loopend
 exit /b 0
-
+rem 根据命理执行对应的c#脚本
 :execute
 	for /f "tokens=1*" %%a in ("%CMD%") do (
 		echo.%%a| find /I ".csx">nul && ( set SCR=%%a ) || ( set SCR=%%a.csx )
